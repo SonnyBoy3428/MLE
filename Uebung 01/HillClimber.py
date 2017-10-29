52c99feddc9aa9fb85aec7feb657727d1fe8b1ac
SonnyBoy3428 <sonnyboy3428@gmail.com>
2017-10-29 02:03:40 +0200

    First try to get it to work. Still need to fully understand how it's supposed to work

--- Uebung 01/HillClimber.py
+++ Uebung 01/HillClimber.py
@@ -1,10 +1,54 @@
+import random
 from random import randint
 
 length = 100
 
-route = [length]
+route = random.sample(range(0, 100), 100)
 distances = [[0 for x in range(length)] for y in range(length)]
 
 
+def print_route():
+    print(''.join(['{:5}'.format(item) for item in route]))
+
+
 def set_distances():
-    distance = randint(0, 100)
+    for x in range(0,length):
+        for y in range (0, length):
+            if x != y:
+                distance = randint(0, length)
+
+                distances[x][y] = distance
+                distances[y][x] = distance
+
+
+def print_distances():
+    print('\n'.join([''.join(['{:5}'.format(item) for item in row]) for row in distances]))
+
+
+def calculate_route():
+    routeSum = int(0)
+    for x in range(0, length - 1):
+        city = route[x]
+        nextCity = route[x + 1]
+        routeSum += distances[city][nextCity]
+
+    return routeSum
+
+
+def swap_cities(index):
+    city1 = route[index]
+    route[index] = route[index+1]
+    route[index+1] = city1
+
+
+set_distances()
+while True:
+    routeSum = calculate_route()
+    for x in range(0, length-1):
+        swap_cities(x)
+        newRouteSum = calculate_route()
+        if newRouteSum <= routeSum:
+            routeSum = newRouteSum
+            print(routeSum)
+        else:
+            swap_cities(x)
